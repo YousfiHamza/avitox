96e8d472e202b726267a961ed5c46807dbea25c7
Hamza Y <yousfihamza22@gmail.com>
2024-09-21 02:58:56 +0200

    fix: build type issue

--- prisma/index.ts
+++ prisma/index.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
+declare global {
+  // Allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
